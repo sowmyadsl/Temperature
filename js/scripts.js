83cd25254a026b21277955845c2cfe289a533c98
Sowmya Dinavahi and Sean Pierce <sean@>
2017-03-20 11:01:32 -0700

    Add conditional statements the script and prompt to print the answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
+// functions
 var fToC = function(far){
   var far = parseInt(prompt("Please enter the Fahrenheit value:"));
   // return far/ (9/5);
   alert((far - 32) * (5/9));
 };
-
-// uncomment the line below to convert F-C
-// fToC();
-
 var cToF = function(Celsius){
 var cel = parseInt(prompt("Please enter the celsius value:"));
 alert("The Fahrenheit value of " + cel + "C" + " is: " + (cel * (9/5) + 32));
 };
 
-// uncomment the line below to convert C-F
-// cToF();
+
+
+var answer = prompt("Are you converting F-C, or C-F?");
+
+if (answer == "F-C") {
+  fToC();
+} else if (answer == "C-F") {
+  cToF();
+} else {
+  alert("Please enter a valid answer ('F-C', or 'C-F')");
+}
